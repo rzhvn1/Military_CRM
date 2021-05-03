0c1fb4d1c364ce739e619dcefb39ba54d19fa6af
rzhvn1 <erzhanmuratov99@gmail.com>
2021-05-03 18:57:38 +0600

    models file for documents app

---
+++ CRM_project/documents/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Document(models.Model):
+    choice = (
+        ('active', 'active'),
+        ('dead', 'dead')
+    )
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    file = models.FileField(blank=True)
+    date_created = models.DateField(auto_now_add=True)
+    date_expired = models.DateField()
+    status = models.CharField(choices=choice, max_length=10)
+    document_root = models.CharField(choices=(
+        ('public', 'public'),
+        ('private', 'private'),
+        ('secret', 'secret'),
+        ('top-secret', 'top-secret'),
+    ),max_length=100)
