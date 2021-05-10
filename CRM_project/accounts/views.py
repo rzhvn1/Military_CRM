55da425a412e7ec0db373a529b08612f63da402b
rzhvn1 <erzhanmuratov99@gmail.com>
2021-05-10 19:29:24 +0600

    views file for accounts app

--- CRM_project/accounts/views.py
+++ CRM_project/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import views, status
 from rest_framework.response import Response
 from .models import *
-from .serializers import DossierSerializer
+from .serializers import DossierSerializer, RegisterSerializer, CarSerializer, EducationSerializer, WarcraftSerializer
 from rest_framework import viewsets
 
 # Create your views here.
@@ -10,3 +10,8 @@ from rest_framework import viewsets
 class DossierModelViewSet(viewsets.ModelViewSet):
     queryset = Dossier.objects.all()
     serializer_class = DossierSerializer
+
+
+class RegisterViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
