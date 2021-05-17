86e55d300f48e7df394c024b530067cbd8985c53
rzhvn1 <erzhanmuratov99@gmail.com>
2021-05-17 19:44:04 +0600

    services file for accounts app

--- CRM_project/accounts/services.py
+++ CRM_project/accounts/services.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
-
+import re
 
 def mailing(username):
     super_users = User.objects.filter(is_superuser=True)
@@ -11,5 +11,32 @@ def mailing(username):
     body = f'User {username} registered in military database, please check him!'
     email = EmailMessage(subject=subject, body=body, to=email_lst)
     email.send()
+
+
+def validate_password(password):
+    while True:
+        if(len(password) <= 8):
+            flag = False
+            break
+        elif not re.search("[a-z]", password):
+            flag = False
+            break
+        elif not re.search("[A-Z]", password):
+            flag = False
+            break
+        elif not re.search("[0-9]", password):
+            flag = False
+            break
+        elif re.search("\s", password):
+            flag = False
+            break
+        else:
+            flag = True
+            break
+
+    if flag == False:
+        return flag
+    else:
+        return flag
 
 
