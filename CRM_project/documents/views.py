04c2df0633920658e89bf065abc6c7e21d10f30f
rzhvn1 <erzhanmuratov99@gmail.com>
2021-05-17 19:45:39 +0600

    views file for documents app

--- CRM_project/documents/views.py
+++ CRM_project/documents/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
-from .permissions import IsSuperUserOrReadOnly, FilterObjPermission
+from .permissions import IsSuperUserOrReadOnly
 from .serializers import *
 # Create your views here.
 
 class DocumentModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsSuperUserOrReadOnly, FilterObjPermission]
+    permission_classes = [IsSuperUserOrReadOnly]
     serializer_class = DocumentSerializer
     filter_backends = [SearchFilter]
     search_fields = ['title']
@@ -26,3 +26,7 @@ class DocumentModelViewSet(viewsets.ModelViewSet):
         else:
             docs = Document.objects.all()
         return docs
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
