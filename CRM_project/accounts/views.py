7843328fe7cb0e807eb749bc909427c7ce5aeceb
rzhvn1 <erzhanmuratov99@gmail.com>
2021-05-17 19:44:42 +0600

    views file for accounts app

--- CRM_project/accounts/views.py
+++ CRM_project/accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import views, status
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from .models import *
 from .serializers import DossierSerializer, RegisterSerializer, CarSerializer, EducationSerializer, WarcraftSerializer
@@ -15,3 +17,13 @@ class DossierModelViewSet(viewsets.ModelViewSet):
 class RegisterViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+class AuthView(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token':token.key})
