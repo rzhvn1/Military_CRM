677c381a221e0f16ba59138570c35bc5ff7389c8
rzhvn1 <erzhanmuratov99@gmail.com>
2021-05-17 19:44:30 +0600

    urls file for accounts app

--- CRM_project/accounts/urls.py
+++ CRM_project/accounts/urls.py
@@ -4,7 +4,9 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('dossier', DossierModelViewSet)
-router.register('register', RegisterViewSet)
+router.register('register', RegisterViewSet, basename="register")
+
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('login/', AuthView.as_view(), name="authorization")
 ]
