e6164a24c1b67b3c77a9880214a1967e8b969afb
rzhvn1 <erzhanmuratov99@gmail.com>
2021-05-19 21:13:09 +0600

    views file for documents

--- CRM_project/documents/views.py
+++ CRM_project/documents/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from .permissions import IsSuperUserOrReadOnly
 from .serializers import *
@@ -11,22 +12,27 @@ class DocumentModelViewSet(viewsets.ModelViewSet):
     filter_backends = [SearchFilter]
     search_fields = ['title']
 
+
+    @action(detail=True, methods=['get'])
     def get_queryset(self):
         try:
             group = self.request.user.groups.all()[0].name
         except IndexError:
-            return Document.objects.filter(document_root__in=['public'])
+            return Document.objects.filter(document_root__in=['public'], status='active')
 
         if group == 'general':
-            docs = Document.objects.filter(document_root__in=['public', 'private', 'secret'])
+            docs = Document.objects.filter(document_root__in=['public', 'private', 'secret'], status='active')
         elif group == 'sergeant':
-            docs = Document.objects.filter(document_root__in=['public', 'private'])
+            docs = Document.objects.filter(document_root__in=['public', 'private'], status='active')
         elif group == 'user':
-            docs = Document.objects.filter(document_root__in=['public'])
+            docs = Document.objects.filter(document_root__in=['public'], status='active')
         else:
             docs = Document.objects.all()
         return docs
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+
 
