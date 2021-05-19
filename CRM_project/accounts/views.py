bfbafda2cd9c958c757b8f0ea40883ef9b02b7d9
rzhvn1 <erzhanmuratov99@gmail.com>
2021-05-19 19:06:43 +0600

    views file for accounts app

--- CRM_project/accounts/views.py
+++ CRM_project/accounts/views.py
@@ -6,12 +6,39 @@ from rest_framework.response import Response
 from .models import *
 from .serializers import DossierSerializer, RegisterSerializer, CarSerializer, EducationSerializer, WarcraftSerializer
 from rest_framework import viewsets
-
+from .serializers import RegisterSerializer
 # Create your views here.
 
-class DossierModelViewSet(viewsets.ModelViewSet):
-    queryset = Dossier.objects.all()
-    serializer_class = DossierSerializer
+class DossierModelViewSet(views.APIView):
+    # serializer_class = DossierSerializer
+    #
+    # def get_queryset(self):
+    #     dossier = Dossier.objects.get(user=self.request.user)
+    #     return dossier
+
+    def get(self, request, *args, **kwargs):
+        try:
+            dossier = Dossier.objects.get(user=request.user)
+        except Dossier.DoesNotExist:
+            return Response({"data":"Dossier doesn't exist"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = RegisterSerializer(dossier)
+        return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        dossier = Dossier.objects.get(user=request.user)
+        serializer = RegisterSerializer(dossier, data=request.user)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("Successfully updated!")
+        return Response(serializer.errors)
+
+    def delete(self, request, *args, **kwargs):
+        dossier = Dossier.objects.get(user=request.user)
+        dossier.delete()
+        return Response({"data":"OK!"})
+
+
+
 
 
 class RegisterViewSet(viewsets.ModelViewSet):
